70660f7a9e1a26b52ab5f025d76e0552a7959fdc
Austin Griffith <you@example.com>
2021-03-23 17:12:16 -0600

    assets

--- packages/react-app/scripts/s3.js
+++ packages/react-app/scripts/s3.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 
 const directoryName = "build";
 
-const BUCKETNAME = "punkwallet.io"; // <<---- SET YOUR BUCKET NAME AND CREATE aws.json ** see below vvvvvvvvvv
+const BUCKETNAME = "gtgs.io"; // <<---- SET YOUR BUCKET NAME AND CREATE aws.json ** see below vvvvvvvvvv
 
 if (!BUCKETNAME) {
   console.log("☢️   Enter a bucket name in packages/react-app/scripts/s3.js ");
@@ -32,7 +32,7 @@ const options = {
 
 // optional cloudfront invalidation rule
 const invalidation = {
- awsDistributionId: "E3D4GB8Y5ZDQB0",
+ awsDistributionId: "E3FRVL4J7ITVPV",
  awsInvalidationPath: "/*"
 }
 
