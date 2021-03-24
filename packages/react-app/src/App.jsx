85ebee91cc22e4084c820e4c7bd84bc1259b4151
Austin Griffith <you@example.com>
2021-03-24 14:43:53 -0600

    poster posting mode

--- packages/react-app/src/App.jsx
+++ packages/react-app/src/App.jsx
@@ -7,7 +7,7 @@ import { Row, Col, Button, Menu, Alert, Switch as SwitchD } from "antd";
 import Web3Modal from "web3modal";
 import WalletConnectProvider from "@walletconnect/web3-provider";
 import { useUserAddress } from "eth-hooks";
-import { useExchangePrice, useGasPrice, useUserProvider, useContractLoader, useContractReader, useEventListener, useBalance, useExternalContractLoader } from "./hooks";
+import { usePoller, useExchangePrice, useGasPrice, useUserProvider, useContractLoader, useContractReader, useEventListener, useBalance, useExternalContractLoader } from "./hooks";
 import { Header, Account, Faucet, Ramp, Contract, GasGauge, ThemeSwitch } from "./components";
 import { Transactor } from "./helpers";
 import { formatEther, parseEther } from "@ethersproject/units";
@@ -15,6 +15,13 @@ import { formatEther, parseEther } from "@ethersproject/units";
 import { Hints, ExampleUI, Subgraph } from "./views"
 import { useThemeSwitcher } from "react-css-theme-switcher";
 import { INFURA_ID, DAI_ADDRESS, DAI_ABI, NETWORK, NETWORKS } from "./constants";
+import ReactScrollWheelHandler from "react-scroll-wheel-handler";
+
+import useSound from 'use-sound';
+import sound1 from './1.mp3';
+import sound2 from './2.mp3';
+import sound4 from './4.mp3';
+import sound5 from './5.mp3';
 /*
     Welcome to 🏗 scaffold-eth !
 
@@ -68,7 +75,7 @@ const blockExplorer = targetNetwork.blockExplorer;
 function App(props) {
 
   const mainnetProvider = (scaffoldEthProvider && scaffoldEthProvider._network) ? scaffoldEthProvider : mainnetInfura
-  if(DEBUG) console.log("🌎 mainnetProvider",mainnetProvider)
+  //if(DEBUG) console.log("🌎 mainnetProvider",mainnetProvider)
 
   const [injectedProvider, setInjectedProvider] = useState();
 /*  const price = useExchangePrice(targetNetwork,mainnetProvider);
@@ -190,18 +197,144 @@ function App(props) {
       </div>
     )
   }
+  <img src="/glow.png" style={{position:"absolute",top:"11%",left:"%70",transform:"scale(5)",opacity:0.5}} />
 */
-  return (
-    <div className="App">
+/*
+  const [ light, setLight ] = useState(false)
 
-      <img src="/ognft.png" style={{marginTop:"10%"}} />
-      <h2 style={{
-        color:"#bbbbbb",
-        fontFamily: "'DotGothic16', sans-serif",
-        letterSpacing:2,
-      }}>4/20 2021</h2>
+  usePoller(()=>{
+    if(Date.now()%16==1){
+      setLight(true)
+      let delay = Math.random()*64+64
+      setTimeout(()=>{
+        setLight(false)
+      },delay)
+      if(Math.random()*64>32){
+        setTimeout(()=>{
+          setLight(true)
+        },delay+30)
+        setTimeout(()=>{
+          setLight(false)
+        },delay+32+Math.random()*32)
+      }
+    }
+  }, 60)*/
+
+  const OGs = 10
+
+  //const [ started, setStarted ] = useState()
+
+  const bounds = [ 678, 1856, 478, 1089 ]
+
+  const randomOG = ()=>{ return Math.floor(Math.random()*OGs)+1 }
+
+  let startingOG
+  while(!startingOG || startingOG == 1){
+    startingOG = randomOG()
+  }
+
+  const [ currentPoster, setCurrentPoster ] = useState( startingOG )//random of posters
+
+  useEffect(()=>{
+    window.scrollTo( window.screen.width/6,window.screen.height/2 );
+  },[])
+
+  const [ renderedThings, setRenderedThings ] = useState([])
+
+  let renderList = []
+
+  const posterSize = 140
+
+  for( let r in renderedThings ){
+    //console.log(renderedThings[r])
+    renderList.push(
+      <div style={{position:"absolute", left: renderedThings[r].x-posterSize/2, top: renderedThings[r].y-(posterSize)/2 }}>
+        <img src={"./"+renderedThings[r].poster+".png"} style={{translate:"rotate()",maxWidth:110}}/>
+      </div>
+    )
+  }
+
+  const [ mouseLocation, setMouseLocation ] = useState()
+  let currentDisplay = ""
+  let valid = false
+  if( currentPoster && mouseLocation ){
+
+    if(mouseLocation[0]>bounds[0] && mouseLocation[0]<bounds[1] && mouseLocation[1]>bounds[2] && mouseLocation[1]<bounds[3] ){
+      valid = true
+    }
+
+    currentDisplay =   (
+      <div style={{position:"absolute", left: mouseLocation[0]-posterSize/2, top: mouseLocation[1]-(posterSize)/2 }}>
+        <img src={"./"+currentPoster+".png"} style={{opacity:valid?0.5:0.1,maxWidth:110}}/>
+      </div>
+    )
+
+  }
+
+  const [playSound1] = useSound(sound1);
+  const [playSound2] = useSound(sound2);
+  const [playSound4] = useSound(sound4);
+  const [playSound5] = useSound(sound5);
+
+  return (
+    <ReactScrollWheelHandler
+        upHandler={(e) => {
+          let prevPoster = currentPoster-1
+          if(prevPoster<=0) prevPoster = OGs;
+          setCurrentPoster(prevPoster)
+        }}
+        downHandler={(e) => {
+          let prevPoster = currentPoster-1
+          if(prevPoster<=0) prevPoster = OGs;
+          setCurrentPoster(prevPoster)
+        }}
+        onMouseMove = {(e) => {
+          setMouseLocation([e.pageX,e.pageY])
+        }}
+    >
+    <div className="App" >
+
+      <div style={{position:"relative"}}>
+        <img src="/animatedPNG.png" style={{width:window.screen.width}}/>
+      </div>
+
+      {renderList}
+
+      {currentDisplay}
+
+      <div onClick={(e)=>{
+        if(valid){
+          console.log("CKLIWDF",e.pageX,e.pageY)
+          if (e.nativeEvent.which === 1) {
+             console.log('Left click');
+           } else if (e.nativeEvent.which === 3) {
+             console.log('Right click');
+           }
+
+            if(Math.random()>0.3){
+              playSound1()
+            }else if(Math.random()>0.2){
+              playSound2()
+            }else if(Math.random()>0.3){
+              playSound4()
+            }else{
+              playSound5()
+            }
+
+            setRenderedThings([...renderedThings,{poster:currentPoster,x:e.pageX, y:e.pageY, r: 1+Math.random()*0.1-Math.random()*0.1, s: 1+Math.random()*0.1-Math.random()*0.1 }])
+            //setCurrentPoster( randomOG() )
+
+        }else{
+
+        }
+
+      }} style={{position:"absolute",left:0,top:0,width:window.screen.width+100, height: window.screen.height+100}}>
+
+      </div>
+
 
     </div>
+    </ReactScrollWheelHandler>
   );
 }
 
