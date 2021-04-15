157923f99a0a0b42244a3ec831d5c5e6391b1120
Austin Griffith <you@example.com>
2021-04-15 11:40:54 -0600

    current version deployed

--- packages/react-app/src/App.jsx
+++ packages/react-app/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
 import "antd/dist/antd.css";
 import {  JsonRpcProvider, Web3Provider } from "@ethersproject/providers";
 import "./App.css";
+import { CaretDownOutlined, CaretRightOutlined } from "@ant-design/icons";
 import { Row, Col, Button, Menu, Alert, Switch as SwitchD } from "antd";
 import Web3Modal from "web3modal";
 import WalletConnectProvider from "@walletconnect/web3-provider";
@@ -42,14 +43,20 @@ import sound5 from './5.mp3';
 */
 
 
+  const OGs = 29//24
+
+
+
+
+
+
 /// 📡 What chain are your contracts deployed to?
 const targetNetwork = NETWORKS['localhost']; // <------- select your target frontend network (localhost, rinkeby, xdai, mainnet)
 
 // 😬 Sorry for all the console logging
 const DEBUG = true
 
-
-
+const newScale = 0.77
 // 🛰 providers
 if(DEBUG) console.log("📡 Connecting to Mainnet Ethereum");
 // const mainnetProvider = getDefaultProvider("mainnet", { infura: INFURA_ID, etherscan: ETHERSCAN_KEY, quorum: 1 });
@@ -70,6 +77,14 @@ const localProvider = mainnetInfura//new JsonRpcProvider(localProviderUrlFromEnv
 
 // 🔭 block explorer URL
 const blockExplorer = targetNetwork.blockExplorer;
+
+const cleanPosterID = (num)=>{
+  //console.log(num)
+  if(num=="6"||num=="13"){
+    return num+"_1"
+  }
+  return num
+}
 
 
 function App(props) {
@@ -220,11 +235,24 @@ function App(props) {
     }
   }, 60)*/
 
-  const OGs = 10
+
+  const [scrollTop, setScrollTop] = useState(0);
+  useEffect(() => {
+    const onScroll = e => {
+      setScrollTop(Math.min(e.target.documentElement.scrollTop,1100));
+      //setScrolling(e.target.documentElement.scrollTop > scrollTop);
+    };
+    window.addEventListener("scroll", onScroll);
+
+    return () => window.removeEventListener("scroll", onScroll);
+  }, [scrollTop]);
+
+console.log("scrollTop",scrollTop)
+
 
   //const [ started, setStarted ] = useState()
 
-  const bounds = [ 678, 1856, 478, 1089 ]
+  const bounds = [ 700, 1936, 512, 1150  ]
 
   const randomOG = ()=>{ return Math.floor(Math.random()*OGs)+1 }
 
@@ -236,10 +264,14 @@ function App(props) {
   const [ currentPoster, setCurrentPoster ] = useState( startingOG )//random of posters
 
   useEffect(()=>{
-    window.scrollTo( window.screen.width/6,window.screen.height/2 );
+    setTimeout(()=>{
+      window.scrollTo( window.screen.width/10, 0 );
+    },500)
+
   },[])
 
   const [ renderedThings, setRenderedThings ] = useState([])
+
 
   let renderList = []
 
@@ -248,8 +280,8 @@ function App(props) {
   for( let r in renderedThings ){
     //console.log(renderedThings[r])
     renderList.push(
-      <div style={{position:"absolute", left: renderedThings[r].x-posterSize/2, top: renderedThings[r].y-(posterSize)/2 }}>
-        <img src={"./"+renderedThings[r].poster+".png"} style={{translate:"rotate()",maxWidth:110}}/>
+      <div key={"poster"+r} style={{opacity:1-0.5*scrollTop/1700,position:"absolute", left: renderedThings[r].x-posterSize/2, top: renderedThings[r].y-(posterSize)/2 }}>
+        <img src={"./"+cleanPosterID(renderedThings[r].poster)+".png"} style={{translate:"rotate()",maxWidth:110}}/>
       </div>
     )
   }
@@ -259,13 +291,13 @@ function App(props) {
   let valid = false
   if( currentPoster && mouseLocation ){
 
-    if(mouseLocation[0]>bounds[0] && mouseLocation[0]<bounds[1] && mouseLocation[1]>bounds[2] && mouseLocation[1]<bounds[3] ){
+    if(mouseLocation[0]>bounds[0]*newScale && mouseLocation[0]<bounds[1]*newScale && mouseLocation[1]>bounds[2]*newScale && mouseLocation[1]<bounds[3]*newScale ){
       valid = true
     }
 
     currentDisplay =   (
       <div style={{position:"absolute", left: mouseLocation[0]-posterSize/2, top: mouseLocation[1]-(posterSize)/2 }}>
-        <img src={"./"+currentPoster+".png"} style={{opacity:valid?0.5:0.1,maxWidth:110}}/>
+        <img src={"./"+cleanPosterID(currentPoster)+".png"} style={{opacity:valid?0.5:0.1,maxWidth:110}}/>
       </div>
     )
 
@@ -275,6 +307,57 @@ function App(props) {
   const [playSound2] = useSound(sound2);
   const [playSound4] = useSound(sound4);
   const [playSound5] = useSound(sound5);
+
+
+
+  const clickFunction = (e)=>{
+    //console.log("CLICK",e)
+
+    let validDoubleCheck = false
+    if(e.pageX>bounds[0]*newScale && e.pageX<bounds[1]*newScale && e.pageY>bounds[2]*newScale && e.pageY<bounds[3]*newScale ){
+      validDoubleCheck = true
+    }
+
+    if(validDoubleCheck){
+      console.log("PASTE IT!",e.pageX,e.pageY)
+
+        let newRenderedThingsShouldBe = [...renderedThings,{poster:currentPoster,x:e.pageX, y:e.pageY, r: 1+Math.random()*0.1-Math.random()*0.1, s: 1+Math.random()*0.1-Math.random()*0.1 }]
+        //console.log("newRenderedThingsShouldBe",newRenderedThingsShouldBe)
+        setRenderedThings( newRenderedThingsShouldBe )
+        setCurrentPoster( randomOG() )
+
+                if(Math.random()>0.3){
+                  playSound1()
+                }else if(Math.random()>0.2){
+                  playSound2()
+                }else if(Math.random()>0.3){
+                  playSound4()
+                }else{
+                  playSound5()
+                }
+    }else{
+
+    }
+
+  }
+
+
+
+  useEffect(()=>{
+    window.addEventListener('touchstart', clickFunction, false);
+  },[ setRenderedThings, renderedThings ])
+
+  const hardSpacer = 16
+  const scheduleZoom = 0.8
+  const scheduleOrigin = "0% 0%"
+  const wallSize = 1300
+
+  const rowStyle = { borderBottom: "8px solid #444444", paddingBottom:hardSpacer }
+  const colStyle = { marginTop:hardSpacer, padding:8, letterSpacing:-1.5 }
+  const posterStyle = { maxWidth:136 }
+
+  const textStyle = { fontFamily:"'Press Start 2P'", fontSize:22 }
+  const secondTextStyle = { fontFamily:"'Press Start 2P'", fontSize:16}
 
   return (
     <ReactScrollWheelHandler
@@ -294,43 +377,297 @@ function App(props) {
     >
     <div className="App" >
 
-      <div style={{position:"relative"}}>
-        <img src="/animatedPNG.png" style={{width:window.screen.width}}/>
+      <div style={{position:"relative",opacity:1-1*scrollTop/1700}}>
+        <img src="/ognftdate.png" style={{width:2660*newScale, height:1540*newScale}}/>
+        {/*<img src="/tv.gif" style={{zIndex:1,position:"absolute",top:1033*newScale,left:517*newScale,maxWidth:240*newScale}} />*/}
+
+        <div style={{position:"fixed",right:32,top:16,zIndex:2}}>
+
+          <img src="./twitter.png" style={{maxWidth:100, cursor:"pointer"}} onClick={()=>{
+            window.open("https://twitter.com/OG_NFT")
+          }}/>
+
+          <img src="./twitch.png" style={{marginLeft:8,maxWidth:80, cursor:"pointer"}} onClick={()=>{
+            window.open("https://www.twitch.tv/ognft")
+          }}/>
+
+          <img src="./youtube.png" style={{marginLeft:26, maxWidth:80, cursor:"pointer"}} onClick={()=>{
+            window.open("https://www.youtube.com/channel/UCtEWm5BNiZJi-2sfcYjGTjw")
+          }}/>
+
+        </div>
       </div>
 
       {renderList}
 
       {currentDisplay}
 
-      <div onClick={(e)=>{
-        if(valid){
-          console.log("CKLIWDF",e.pageX,e.pageY)
-          if (e.nativeEvent.which === 1) {
-             console.log('Left click');
-           } else if (e.nativeEvent.which === 3) {
-             console.log('Right click');
-           }
-
-            if(Math.random()>0.3){
-              playSound1()
-            }else if(Math.random()>0.2){
-              playSound2()
-            }else if(Math.random()>0.3){
-              playSound4()
-            }else{
-              playSound5()
-            }
-
-            setRenderedThings([...renderedThings,{poster:currentPoster,x:e.pageX, y:e.pageY, r: 1+Math.random()*0.1-Math.random()*0.1, s: 1+Math.random()*0.1-Math.random()*0.1 }])
-            //setCurrentPoster( randomOG() )
-
-        }else{
-
-        }
-
-      }} style={{position:"absolute",left:0,top:0,width:window.screen.width+100, height: window.screen.height+100}}>
+      <div onTouchStart={clickFunction}  onClick={clickFunction} style={{position:"absolute",left:0,top:0,width:window.screen.width+100, height: window.screen.height+100}}>
 
       </div>
+
+      <div style={{position:"absolute",opacity:1,top:1900-Math.min(2000,scrollTop*1.6),left:500}}>
+        <img src="./middlebuilding.png" style={{minWidth:wallSize*0.9}} />
+      </div>
+
+      <div style={{position:"absolute",top:2000-scrollTop,left:400}}>
+        <div><img src="./bdl.png" style={{minWidth:wallSize}} /></div>
+        <div><img src="./bdl2.png" style={{minWidth:wallSize}} /></div>
+      </div>
+
+      {/* <div style={{position:"absolute",top:400-scrollTop,left:280, fontFamily:"'Press Start 2P'", fontSize:16,color:"#ffffff" }}> MINT NFTs  <CaretRightOutlined /></div> */}
+
+      <div style={{position:"absolute",top:600-scrollTop,left:280, fontFamily:"'Press Start 2P'", fontSize:16,color:"#ffffff" }}> SCHEDULE  <CaretDownOutlined /></div>
+
+      {/* <div style={{position:"absolute",top:800-scrollTop,left:1580, fontFamily:"'Press Start 2P'", fontSize:16,color:"#ffffff" }}> <Button type="primary" onClick={()=>{alert("coming soon ;)")}}>MINT NFT</Button></div>
+*/}
+
+      <div style={{position:"absolute",top:2100-scrollTop,left:522,width:1448,transform:"scale("+scheduleZoom+")",transformOrigin:scheduleOrigin}}>
+
+        <Row style={rowStyle} >
+          <Col span={4} style={colStyle}>
+            <div style={textStyle}>INTRO</div>
+            <div style={secondTextStyle}>~ 11:00 AM ET</div>
+            <div style={secondTextStyle}>4/20</div>
+          </Col>
+          <Col  style={colStyle}>
+            <img src={"./4.png"} style={posterStyle}/>
+          </Col>
+          <Col style={colStyle}>
+            <img src={"./9.png"} style={posterStyle}/>
+          </Col>
+        </Row>
+
+        <Row style={rowStyle} >
+          <Col span={4} style={colStyle}>
+            <div style={textStyle}>TBD</div>
+            <div style={secondTextStyle}>~ 11:15 AM ET</div>
+          </Col>
+          <Col  style={colStyle}>
+            <img src={"./8.png"} style={posterStyle}/>
+          </Col>
+          <Col span={4} style={colStyle}>
+            <div style={textStyle}>Lessons From Early Days of Ujo</div>
+            <div style={secondTextStyle}>~ 11:30 AM ET</div>
+          </Col>
+          <Col  style={colStyle}>
+            <img src={"./2.png"} style={posterStyle}/>
+          </Col>
+          <Col span={4} style={colStyle}>
+            <div style={textStyle}>Behavioral Economics and NFTs (Live Experiment!!!)</div>
+            <div style={secondTextStyle}>~ 11:45 AM ET</div>
+          </Col>
+          <Col  style={colStyle}>
+            <img src={"./13_1.png"} style={posterStyle}/>
+          </Col>
+        </Row>
+
+
+
+        <Row style={rowStyle} >
+          <Col span={4} style={colStyle}>
+            <div style={textStyle}>OG PANEL</div>
+            <div style={secondTextStyle}>~ 12:00 PM ET</div>
+          </Col>
+          <Col  style={colStyle}>
+            <img src={"./5.png"} style={posterStyle}/>
+          </Col>
+          <Col style={colStyle}>
+            <img src={"./4.png"} style={posterStyle}/>
+          </Col>
+          <Col  style={colStyle}>
+            <img src={"./2.png"} style={posterStyle}/>
+          </Col>
+          <Col style={colStyle}>
+            <img src={"./8.png"} style={posterStyle}/>
+          </Col>
+          <Col span={4} style={colStyle}>
+            <div style={textStyle}>An Introduction to Flowertokens</div>
+            <div style={secondTextStyle}>~ 12:45 PM ET</div>
+          </Col>
+          <Col  style={colStyle}>
+             <img src={"./29.png"} style={posterStyle}/>
+          </Col>
+        </Row>
+
+
+        <Row style={rowStyle} >
+
+          <Col span={6} style={colStyle}>
+            <div style={textStyle}>How It Started ... How It’s Going</div>
+            <div style={secondTextStyle}>~ 1:00 PM ET</div>
+          </Col>
+          <Col  style={colStyle}>
+             <img src={"./30.png"} style={posterStyle}/>
+          </Col>
+          <Col span={6} style={colStyle}>
+            <div style={textStyle}>Wanderer Above a Sea of FUD</div>
+            <div style={secondTextStyle}>~ 1:15 PM ET</div>
+          </Col>
+          <Col  style={colStyle}>
+            <img src={"./5.png"} style={posterStyle}/>
+          </Col>
+          <Col style={colStyle}>
+            <img src={"./27.png"} style={posterStyle}/>
+          </Col>
+        </Row>
+
+        <Row style={rowStyle} >
+          <Col span={6} style={colStyle}>
+            <div style={textStyle}>Evolution of Cryptoart</div>
+            <div style={secondTextStyle}>~ 1:30 PM ET</div>
+          </Col>
+          <Col  style={colStyle}>
+            <img src={"./11.png"} style={posterStyle}/>
+          </Col>
+          <Col style={colStyle}>
+            <img src={"./16.png"} style={posterStyle}/>
+          </Col>
+          <Col style={colStyle}>
+            <img src={"./27.png"} style={posterStyle}/>
+          </Col>
+        </Row>
+
+
+        <Row style={rowStyle} >
+          <Col span={4} style={colStyle}>
+            <div style={textStyle}>AUDIO PANEL</div>
+            <div style={secondTextStyle}>~ 2:00 PM ET</div>
+          </Col>
+          <Col  style={colStyle}>
+            <img src={"./6_1.png"} style={posterStyle}/>
+          </Col>
+          <Col  style={colStyle}>
+            <img src={"./25.png"} style={posterStyle}/>
+          </Col>
+          <Col  style={colStyle}>
+            <img src={"./28.png"} style={posterStyle}/>
+          </Col>
+          <Col  style={colStyle}>
+            <img src={"./2.png"} style={posterStyle}/>
+          </Col>
+          <Col  style={colStyle}>
+            <img src={"./26.png"} style={posterStyle}/>
+          </Col>
+        </Row>
+
+
+        <Row style={rowStyle} >
+          <Col span={4} style={colStyle}>
+            <div style={textStyle}>FUTURE PANEL</div>
+            <div style={secondTextStyle}>~ 2:30 PM ET</div>
+          </Col>
+          <Col  style={colStyle}>
+            <img src={"./17.png"} style={posterStyle}/>
+          </Col>
+          <Col  style={colStyle}>
+            <img src={"./14.png"} style={posterStyle}/>
+          </Col>
+          <Col  style={colStyle}>
+            <img src={"./15.png"} style={posterStyle}/>
+          </Col>
+          <Col  style={colStyle}>
+            <img src={"./7.png"} style={posterStyle}/>
+          </Col>
+          <Col style={colStyle}>
+            <img src={"./3.png"} style={posterStyle}/>
+          </Col>
+        </Row>
+
+
+        <Row style={rowStyle} >
+          <Col span={6} style={colStyle}>
+            <div style={textStyle}>The Curse of Non-Fungibility</div>
+            <div style={secondTextStyle}>~ 3:00 PM ET</div>
+          </Col>
+          <Col  style={colStyle}>
+            <img src={"./10.png"} style={posterStyle}/>
+          </Col>
+          <Col span={6} style={colStyle}>
+            <div style={textStyle}>TBD</div>
+            <div style={secondTextStyle}>~ 3:15 PM ET</div>
+          </Col>
+          <Col  style={colStyle}>
+            <img src={"./12.png"} style={posterStyle}/>
+          </Col>
+        </Row>
+
+        <Row style={rowStyle} >
+          <Col span={6} style={colStyle}>
+            <div style={textStyle}>TBD</div>
+            <div style={secondTextStyle}>~ 3:30 PM ET</div>
+          </Col>
+          <Col  style={colStyle}>
+            <img src={"./11.png"} style={posterStyle}/>
+          </Col>
+          <Col span={6} style={colStyle}>
+            <div style={textStyle}>Notes on the Synthesis of Digital Form</div>
+            <div style={secondTextStyle}>~ 3:45 PM ET</div>
+          </Col>
+          <Col  style={colStyle}>
+            <img src={"./4.png"} style={posterStyle}/>
+          </Col>
+        </Row>
+
+
+        <Row style={rowStyle} >
+          <Col span={4} style={colStyle}>
+            <div style={textStyle}>ERC721 ORIGIN STORY</div>
+            <div style={secondTextStyle}>~ 4PM ET</div>
+          </Col>
+          <Col  style={colStyle}>
+            <img src={"./19.png"} style={posterStyle}/>
+          </Col>
+          <Col style={colStyle}>
+            <img src={"./18.png"} style={posterStyle}/>
+          </Col>
+          <Col  style={colStyle}>
+            <img src={"./20.png"} style={posterStyle}/>
+          </Col>
+          <Col style={colStyle}>
+            <img src={"./21.png"} style={posterStyle}/>
+          </Col>
+          <Col  style={colStyle}>
+            <img src={"./22.png"} style={posterStyle}/>
+          </Col>
+          <Col style={colStyle}>
+            <img src={"./23.png"} style={posterStyle}/>
+          </Col>
+          <Col  style={colStyle}>
+            <img src={"./24.png"} style={posterStyle}/>
+          </Col>
+
+        </Row>
+
+        <Row style={rowStyle} >
+          <Col span={4} style={colStyle}>
+            <div style={textStyle}>CLOSING</div>
+            <div style={secondTextStyle}>~ 4:45 PM ET</div>
+            <div style={secondTextStyle}>4/20</div>
+          </Col>
+          <Col  style={colStyle}>
+            <img src={"./9.png"} style={posterStyle}/>
+          </Col>
+        </Row>
+
+
+        <div style={{marginLeft:-100,position:"relative"}}>
+
+          <img src="./twitter.png" style={{maxWidth:300, cursor:"pointer",marginTop:-16,marginRight:8}} onClick={()=>{
+            window.open("https://twitter.com/OG_NFT")
+          }}/>
+
+          <img src="./twitch.png" style={{maxWidth:190, cursor:"pointer"}} onClick={()=>{
+            window.open("https://www.twitch.tv/ognft")
+          }}/>
+
+          <img src="./youtube.png" style={{marginLeft:64, maxWidth:160, cursor:"pointer"}} onClick={()=>{
+            window.open("https://www.youtube.com/channel/UCtEWm5BNiZJi-2sfcYjGTjw")
+          }}/>
+
+        </div>
+      </div>
+
 
 
     </div>
@@ -362,10 +699,15 @@ const logoutOfWeb3Modal = async () => {
   }, 1);
 };
 
- window.ethereum && window.ethereum.on('chainChanged', chainId => {
+window.ethereum && window.ethereum.on('chainChanged', chainId => {
   setTimeout(() => {
     window.location.reload();
   }, 1);
 })
+/*
+window.addEventListener('t ouchstart', function onFirstTouch() {
+  alert("you")
+}, false);
+*/
 
 export default App;
