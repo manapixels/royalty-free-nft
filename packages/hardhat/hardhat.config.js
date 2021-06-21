1dbb5aaa4c4f5e51a7fa469811d23c6a8951c23a
viraj124 <virajm72@gmail.com>
2021-06-21 16:21:28 +0530

    initial contract setup

--- packages/hardhat/hardhat.config.js
+++ packages/hardhat/hardhat.config.js
@@ -107,6 +107,15 @@ module.exports = {
         }
       },
       {
+        version: "0.7.1",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200
+          }
+        }
+      },
+      {
         version: "0.6.7",
         settings: {
           optimizer: {
