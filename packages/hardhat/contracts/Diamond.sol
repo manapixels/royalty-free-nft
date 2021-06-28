77e482fa6482ebdcb7cbea30e3cae65772529298
viraj124 <virajm72@gmail.com>
2021-06-28 22:27:03 +0530

    refactor: added lopupe facet and code clean up

--- packages/hardhat/contracts/Diamond.sol
+++ packages/hardhat/contracts/Diamond.sol
@@ -12,16 +12,7 @@ import "./libraries/LibDiamond.sol";
 import "./interfaces/IDiamondLoupe.sol";
 import "./interfaces/IDiamondCut.sol";
 import "./interfaces/IERC173.sol";
-
-interface IERC165 {
-    /// @notice Query if a contract implements an interface
-    /// @param interfaceId The interface identifier, as specified in ERC-165
-    /// @dev Interface identification is specified in ERC-165. This function
-    ///  uses less than 30,000 gas.
-    /// @return `true` if the contract implements `interfaceID` and
-    ///  `interfaceID` is not 0xffffffff, `false` otherwise
-    function supportsInterface(bytes4 interfaceId) external view returns (bool);
-}
+import "./interfaces/IERC165.sol";
 
 contract Diamond {
     constructor(IDiamondCut.FacetCut[] memory _diamondCut) payable {
