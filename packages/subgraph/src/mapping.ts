102379916967087c78ba35789e5825721870ca5f
Miguel Ortiz <miaortizma@unal.edu.co>
2021-08-14 19:31:47 -0500

    testing graph codegen

--- packages/subgraph/src/mapping.ts
+++ packages/subgraph/src/mapping.ts
@@ -1,34 +1,53 @@
-import { BigInt, Address } from "@graphprotocol/graph-ts"
 import {
-  YourContract,
-  SetPurpose
-} from "../generated/YourContract/YourContract"
-import { Purpose, Sender } from "../generated/schema"
+  Approval as ApprovalEvent,
+  ApprovalForAll as ApprovalForAllEvent,
+  OwnershipTransferred as OwnershipTransferredEvent,
+  Transfer as TransferEvent
+} from "../generated/IpNft_subgraph/IpNft_subgraph"
+import {
+  Approval,
+  ApprovalForAll,
+  OwnershipTransferred,
+  Transfer
+} from "../generated/schema"
 
-export function handleSetPurpose(event: SetPurpose): void {
+export function handleApproval(event: ApprovalEvent): void {
+  let entity = new Approval(
+    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
+  )
+  entity.owner = event.params.owner
+  entity.approved = event.params.approved
+  entity.tokenId = event.params.tokenId
+  entity.save()
+}
 
-  let senderString = event.params.sender.toHexString()
+export function handleApprovalForAll(event: ApprovalForAllEvent): void {
+  let entity = new ApprovalForAll(
+    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
+  )
+  entity.owner = event.params.owner
+  entity.operator = event.params.operator
+  entity.approved = event.params.approved
+  entity.save()
+}
 
-  let sender = Sender.load(senderString)
+export function handleOwnershipTransferred(
+  event: OwnershipTransferredEvent
+): void {
+  let entity = new OwnershipTransferred(
+    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
+  )
+  entity.previousOwner = event.params.previousOwner
+  entity.newOwner = event.params.newOwner
+  entity.save()
+}
 
-  if (sender == null) {
-    sender = new Sender(senderString)
-    sender.address = event.params.sender
-    sender.createdAt = event.block.timestamp
-    sender.purposeCount = BigInt.fromI32(1)
-  }
-  else {
-    sender.purposeCount = sender.purposeCount.plus(BigInt.fromI32(1))
-  }
-
-  let purpose = new Purpose(event.transaction.hash.toHex() + "-" + event.logIndex.toString())
-
-  purpose.purpose = event.params.purpose
-  purpose.sender = senderString
-  purpose.createdAt = event.block.timestamp
-  purpose.transactionHash = event.transaction.hash.toHex()
-
-  purpose.save()
-  sender.save()
-
+export function handleTransfer(event: TransferEvent): void {
+  let entity = new Transfer(
+    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
+  )
+  entity.from = event.params.from
+  entity.to = event.params.to
+  entity.tokenId = event.params.tokenId
+  entity.save()
 }
